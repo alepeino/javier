4c7375da5c87bbd73fad71018bf7d93903e6800e
Alejandro Peinó <alepeino@gmail.com>
2022-02-06 12:23:56 -0300

    react+remix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,17 +15,25 @@ module.exports = {
   plugins: ['@typescript-eslint', 'prettier'],
   extends: [
     'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
     'plugin:import/errors',
     'plugin:import/warnings',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    '@remix-run/eslint-config',
+    '@remix-run/eslint-config/jest',
   ],
   rules: {
     'no-unused-vars': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '_' }],
     '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/consistent-type-imports': [
+      1,
+      { prefer: 'type-imports' },
+    ],
     'no-console': 'warn',
     'import/no-unresolved': 0,
     'import/order': [
@@ -34,5 +42,10 @@ module.exports = {
         alphabetize: { order: 'asc' },
       },
     ],
+    'react/jsx-curly-brace-presence': [
+      1,
+      { props: 'never', children: 'never' },
+    ],
+    'react/react-in-jsx-scope': 0,
   },
-};
+}
