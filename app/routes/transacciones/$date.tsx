6f2a6746ea968a998e309ee8faa7cb1a8fb7ac6c
Alejandro Peinó <alepeino@gmail.com>
2022-02-14 18:41:05 -0300

    loading indicator

--- app/routes/transacciones/$date.tsx
+++ app/routes/transacciones/$date.tsx
@@ -5,6 +5,7 @@ import {
   Heading,
   HStack,
   IconButton,
+  Progress,
   Stack,
   Stat,
   StatHelpText,
@@ -15,7 +16,7 @@ import {
 import { addDays, subDays } from 'date-fns/fp'
 import { sumBy } from 'lodash/fp'
 import type { LoaderFunction } from 'remix'
-import { Link, useLoaderData } from 'remix'
+import { Link, useLoaderData, useTransition } from 'remix'
 import { TransactionTable } from '~/components/TransactionTable'
 import { dateInYMD, fromYMD } from '~/lib/date'
 import { currencyFormatter, dayFormatter } from '~/lib/intl-format'
@@ -48,6 +49,7 @@ export const loader: LoaderFunction = async ({ params }) => {
 }
 
 export default function TransactionsIndex() {
+  const transition = useTransition()
   const { date, transactions } = useLoaderData<LoaderData>()
   const day = fromYMD(date)
   const dayBefore = subDays(1, day)
@@ -56,81 +58,94 @@ export default function TransactionsIndex() {
   const registerHref = `../registrar?date=${date}`
 
   return (
-    <Stack spacing={4}>
-      <HStack justifyContent="space-between">
-        <Link to={`../${dateInYMD(dayBefore)}`} prefetch="intent">
-          <IconButton
-            as="span"
-            colorScheme="purple"
-            aria-label={dayFormatter.format(dayBefore)}
-            icon={<ArrowLeftIcon />}
-            borderRadius="100%"
-          />
-        </Link>
-        <Heading size="lg" as="h1" aria-describedby="title-description">
-          {dayFormatter.format(day)}
-        </Heading>
-        <Link to={`../${dateInYMD(dayAfter)}`} prefetch="intent">
-          <IconButton
-            as="span"
-            colorScheme="purple"
-            aria-label={dayFormatter.format(dayAfter)}
-            icon={<ArrowRightIcon />}
-            borderRadius="100%"
-          />
-        </Link>
-      </HStack>
-      <main>
-        {transactions.length ? (
-          <Stack spacing={8}>
-            <Stat textAlign="center">
-              <StatLabel>Total del día (ARS):</StatLabel>
-              <StatNumber>
-                {currencyFormatter.format(sumBy('deltaARS', transactions))}
-              </StatNumber>
-              <StatHelpText id="title-description">
-                {transactions.length}{' '}
-                {transactions.length > 1 ? 'transacciones' : 'transacción'}
-              </StatHelpText>
-            </Stat>
-
-            <Box overflow="auto">
-              <TransactionTable transactions={transactions} />
-            </Box>
-          </Stack>
-        ) : (
-          <Text id="title-description">
-            No hay transacciones registradas en esta fecha
-          </Text>
-        )}
-
-        <Box display={{ base: 'block', md: 'none' }}>
-          <Link to={registerHref}>
+    <>
+      {transition.location && (
+        <Progress
+          size="sm"
+          colorScheme="primary"
+          isIndeterminate
+          position="fixed"
+          top={0}
+          left={0}
+          right={0}
+        />
+      )}
+      <Stack spacing={4}>
+        <HStack justifyContent="space-between">
+          <Link to={`../${dateInYMD(dayBefore)}`} prefetch="intent">
             <IconButton
               as="span"
-              size="lg"
-              colorScheme="primary"
-              aria-label="Registrar transacción"
-              icon={<AddIcon />}
+              colorScheme="purple"
+              aria-label={dayFormatter.format(dayBefore)}
+              icon={<ArrowLeftIcon />}
               borderRadius="100%"
-              position="absolute"
-              bottom="1rem"
-              right="1rem"
             />
           </Link>
-        </Box>
-        <Box
-          display={{ base: 'none', md: 'flex' }}
-          mt={4}
-          justifyContent="flex-end"
-        >
-          <Link to={registerHref}>
-            <Button as="span" colorScheme="primary">
-              Registrar transacción
-            </Button>
+          <Heading size="lg" as="h1" aria-describedby="title-description">
+            {dayFormatter.format(day)}
+          </Heading>
+          <Link to={`../${dateInYMD(dayAfter)}`} prefetch="intent">
+            <IconButton
+              as="span"
+              colorScheme="purple"
+              aria-label={dayFormatter.format(dayAfter)}
+              icon={<ArrowRightIcon />}
+              borderRadius="100%"
+            />
           </Link>
-        </Box>
-      </main>
-    </Stack>
+        </HStack>
+        <main>
+          {transactions.length ? (
+            <Stack spacing={8}>
+              <Stat textAlign="center">
+                <StatLabel>Total del día (ARS):</StatLabel>
+                <StatNumber>
+                  {currencyFormatter.format(sumBy('deltaARS', transactions))}
+                </StatNumber>
+                <StatHelpText id="title-description">
+                  {transactions.length}{' '}
+                  {transactions.length > 1 ? 'transacciones' : 'transacción'}
+                </StatHelpText>
+              </Stat>
+
+              <Box overflow="auto">
+                <TransactionTable transactions={transactions} />
+              </Box>
+            </Stack>
+          ) : (
+            <Text id="title-description">
+              No hay transacciones registradas en esta fecha
+            </Text>
+          )}
+
+          <Box display={{ base: 'block', md: 'none' }}>
+            <Link to={registerHref}>
+              <IconButton
+                as="span"
+                size="lg"
+                colorScheme="primary"
+                aria-label="Registrar transacción"
+                icon={<AddIcon />}
+                borderRadius="100%"
+                position="absolute"
+                bottom="1rem"
+                right="1rem"
+              />
+            </Link>
+          </Box>
+          <Box
+            display={{ base: 'none', md: 'flex' }}
+            mt={4}
+            justifyContent="flex-end"
+          >
+            <Link to={registerHref}>
+              <Button as="span" colorScheme="primary">
+                Registrar transacción
+              </Button>
+            </Link>
+          </Box>
+        </main>
+      </Stack>
+    </>
   )
 }
