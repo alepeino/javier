08b6b61e0951158dd182a84dd638bc876c08ce85
Alejandro Peinó <alepeino@gmail.com>
2022-02-14 00:00:06 -0300

    empty state

--- app/routes/transacciones/$date.tsx
+++ app/routes/transacciones/$date.tsx
@@ -1,5 +1,5 @@
 import { AddIcon } from '@chakra-ui/icons'
-import { Box, Button, Heading, IconButton } from '@chakra-ui/react'
+import { Box, Button, Heading, IconButton, Text } from '@chakra-ui/react'
 import type { LoaderFunction } from 'remix'
 import { Link, useLoaderData } from 'remix'
 import { TransactionTable } from '~/components/TransactionTable'
@@ -39,35 +39,41 @@ export default function TransactionsIndex() {
     <>
       <Heading>{date}</Heading>
       <main>
-        <Box overflow="auto">
-          <TransactionTable transactions={transactions} />
-        </Box>
-        <Box display={{ base: 'block', md: 'none' }}>
-          <Link to="../registrar">
-            <IconButton
-              as="span"
-              size="lg"
-              colorScheme="primary"
-              aria-label="Registrar transacción"
-              icon={<AddIcon />}
-              borderRadius="100%"
-              position="absolute"
-              bottom="1rem"
-              right="1rem"
-            />
-          </Link>
-        </Box>
-        <Box
-          display={{ base: 'none', md: 'flex' }}
-          mt={4}
-          justifyContent="flex-end"
-        >
-          <Link to="../registrar">
-            <Button as="span" colorScheme="primary">
-              Registrar transacción
-            </Button>
-          </Link>
-        </Box>
+        {transactions.length ? (
+          <>
+            <Box overflow="auto">
+              <TransactionTable transactions={transactions} />
+            </Box>
+            <Box display={{ base: 'block', md: 'none' }}>
+              <Link to="../registrar">
+                <IconButton
+                  as="span"
+                  size="lg"
+                  colorScheme="primary"
+                  aria-label="Registrar transacción"
+                  icon={<AddIcon />}
+                  borderRadius="100%"
+                  position="absolute"
+                  bottom="1rem"
+                  right="1rem"
+                />
+              </Link>
+            </Box>
+            <Box
+              display={{ base: 'none', md: 'flex' }}
+              mt={4}
+              justifyContent="flex-end"
+            >
+              <Link to="../registrar">
+                <Button as="span" colorScheme="primary">
+                  Registrar transacción
+                </Button>
+              </Link>
+            </Box>
+          </>
+        ) : (
+          <Text>No hay transacciones registradas en esta fecha</Text>
+        )}
       </main>
     </>
   )
